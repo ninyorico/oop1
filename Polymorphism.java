35326c7dc4b984b4aa2622c84d47516a16863490
Niño Bancoro <143783481+ninyorico@users.noreply.github.com>
2024-11-22 14:53:59 +0800

    Add files via upload

---
+++ Polymorphism.java
@@ -0,0 +1,17 @@
+public class Polymorphism{
+    public static void main(String[] args) {
+    Student student = new Student("Niño", "City of Calaca, Batangas", "BSIT",2);
+    System.out.println("Student Name: " + student.getName());
+    System.out.println("Student Address: " + student.getAddress());
+    System.out.println("Student Program: " + student.getProgram());
+    System.out.println("Student Year: " + student.getYear());
+    System.out.println(student.toString());
+    System.out.println("");
+    Staff staff = new Staff("Mat", "City of Calaca, Dacanlao, Batangas", "BSU",500);
+    System.out.println("Staff Name: " + staff.getName());
+    System.out.println("Staff Address: " + staff.getAddress());
+    System.out.println("Staff School: " + staff.getSchool());
+    System.out.printf("Staff Salary: %.2f \n",staff.getSalary());
+    System.out.println(staff.toString());
+}
+}
